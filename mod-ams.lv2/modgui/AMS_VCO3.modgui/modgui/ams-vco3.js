8cb03fa92707756ecbb5024d93a2067ee328ad17
blushi <marie.gauthier63@gmail.com>
2019-05-23 16:32:28 +0200

    Fix strange behavior running multiple instances of AMS VCO3

--- mod-ams.lv2/modgui/AMS_VCO3.modgui/modgui/ams-vco3.js
+++ mod-ams.lv2/modgui/AMS_VCO3.modgui/modgui/ams-vco3.js
@@ -1,12 +1,22 @@
 function (event) {
-  if (event.type == 'change') {
-    var ledLine = document.getElementById("led-line")
-    if (event.symbol === "waveForm") {
-      if (event.value === 2 || event.value === 3) {
-        ledLine.style.backgroundPosition = "left center";
-      } else {
-        ledLine.style.backgroundPosition = "right center";
+  var icon = event.icon;
+  if (icon.length) {
+    var instance = icon[0].getAttribute("mod-instance");
+    var modPedals = document.getElementsByClassName("mod-pedal");
+    for (i = 0; i < modPedals.length; i++) {
+      var modPedal = modPedals[i];
+      if (modPedal.getAttribute("mod-instance") === instance) {
+        if (event.type == 'change') {
+          var ledLine = modPedal.getElementsByClassName("led-line")[0];
+          if (event.symbol === "waveForm") {
+            if (event.value === 2 || event.value === 3) {
+              ledLine.style.backgroundPosition = "left center";
+            } else {
+              ledLine.style.backgroundPosition = "right center";
+            }
+          }
+        }
       }
     }
-	}
+  }
 }
