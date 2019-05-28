486fc7120e242ddd01d6e9cb466df26a95625aeb
blushi <marie.gauthier63@gmail.com>
2019-05-28 09:56:31 +0200

    Get current VCO3 instance directly from 'icon' in js

--- mod-ams.lv2/modgui/AMS_VCO3.modgui/modgui/ams-vco3.js
+++ mod-ams.lv2/modgui/AMS_VCO3.modgui/modgui/ams-vco3.js
@@ -1,21 +1,20 @@
 function (event) {
-  var icon = event.icon;
-  if (icon.length) {
-    var instance = icon[0].getAttribute("mod-instance");
-    var modPedals = document.getElementsByClassName("mod-pedal");
-    for (i = 0; i < modPedals.length; i++) {
-      var modPedal = modPedals[i];
-      if (modPedal.getAttribute("mod-instance") === instance) {
-        if (event.type == 'change') {
-          var ledLine = modPedal.getElementsByClassName("led-line")[0];
-          if (event.symbol === "waveForm") {
-            if (event.value === 2 || event.value === 3) {
-              ledLine.style.backgroundPosition = "left center";
-            } else {
-              ledLine.style.backgroundPosition = "right center";
-            }
-          }
-        }
+  function setPosition (newPosition) {
+    var led = event.icon.find ('[mod-role=waveform-led]');
+    var oldPosition = led.data('xBgPosition');
+    if (oldPosition === newPosition) {
+      return;
+    }
+    led.css({ backgroundPosition: newPosition});
+    led.data ('xBgPosition', newPosition);
+  }
+
+  if (event.type == 'change') {
+    if (event.symbol === "waveForm") {
+      if (event.value === 2 || event.value === 3) {
+        setPosition("left center");
+      } else {
+        setPosition("right center");
       }
     }
   }
